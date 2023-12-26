48f1e25d2359bd53688672e3df136babc9d1460d
Afsana Ansari <77285521+Afsana2707@users.noreply.github.com>
2023-12-26 13:49:35 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+  });
+
+  Shery.makeMagnet(".magnet");
+ Shery.hoverWithMediaCircle(".hvr", {images: ["./image3.jpeg", "./image2.jpeg", "./image1.jpeg" ]});
+ 
